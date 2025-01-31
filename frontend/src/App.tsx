20e14c1bb0c2b1ef0ce0ba843d0e49b9f1213c3f
FedericoIglesias <igl.fede@gmail.com>
2025-01-31 17:02:35 +0100

    refactor

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -23,30 +23,7 @@ const Aside = styled.aside`
   }
 `;
 
-const AddEntrance = styled.main`
-  display: flex;
-  flex-direction: column;
-  padding: 20px;
-  input{
-    margin-bottom: 10px;
-    width: 200px;
-  }
-  select{
-    margin-bottom: 10px;
-    width: 200px;
-  }
-  button{
-    width: 100px;
-    padding: 10px;
-    background-color: #f1f1f1;
-    border: 1px solid black;
-    border-radius: 5px;
-    &:hover{
-      cursor: pointer;
-      background-color: #d1d1d1;
-    }
-  }
-`;
+
 
 export function App() {
   return (
@@ -58,21 +35,7 @@ export function App() {
         <ul></ul>
         <ul></ul>
       </Aside>
-      <AddEntrance>
-        <h1>Agregar Entrada</h1>
-        <label htmlFor="">Valor</label>
-        <input type="number" name="" id="" />
-        <label htmlFor="">Concepto:</label>
-        <input type="text" name="" id="" />
-        <label htmlFor="">Fecha:</label>
-        <input type="date" name="" id="" />
-        <select name="" id="">
-          <option value=""></option>
-          <option value="">Gasto</option>
-          <option value="">Ingreso</option>
-        </select>
-        <button>Guardar</button>
-      </AddEntrance>
+      
     </Body>
   );
 }
---
+++ frontend/src/components/AddEntrance.tsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+
+const AddEntranceStyle = styled.main`
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  input {
+    margin-bottom: 10px;
+    width: 200px;
+  }
+  select {
+    margin-bottom: 10px;
+    width: 200px;
+  }
+  button {
+    width: 100px;
+    padding: 10px;
+    background-color: #f1f1f1;
+    border: 1px solid black;
+    border-radius: 5px;
+    &:hover {
+      cursor: pointer;
+      background-color: #d1d1d1;
+    }
+  }
+`;
+
+export const AddEntrance = () => {
+  return (
+    <AddEntranceStyle>
+      <h1>Agregar Entrada</h1>
+      <label htmlFor="">Valor:</label>
+      <input type="number" name="" id="" />
+      <label htmlFor="">Concepto:</label>
+      <input type="text" name="" id="" />
+      <label htmlFor="">Fecha:</label>
+      <input type="date" name="" id="" />
+      <select name="" id="">
+        <option value=""></option>
+        <option value="">Gasto</option>
+        <option value="">Ingreso</option>
+      </select>
+      <button>Guardar</button>
+    </AddEntranceStyle>
+  );
+};
