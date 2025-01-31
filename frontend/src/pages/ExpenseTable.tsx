775abb353c9de6c8832d530e5ad6d0f94b9711c0
FedericoIglesias <igl.fede@gmail.com>
2025-01-31 17:44:48 +0100

    styles

--- frontend/src/pages/ExpenseTable.tsx
+++ frontend/src/pages/ExpenseTable.tsx
@@ -1,13 +1,48 @@
-import { Table } from "../components/Table"
-import entry from "../data/entry.json"
+import styled from "styled-components";
+import { Table } from "../components/Table";
+import entry from "../data/entry.json";
+
+const ExpenseTableStyle = styled.main`
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  h1 {
+    margin-bottom: 20px;
+  }
+  Table {
+    margin-top: 20px;
+    thead {
+      background-color: #84b5c6;
+      padding: 10px;
+    }
+    tbody {
+      padding: 10px;
+      tr {
+        &:nth-child(even){
+          background-color: #d1d1d1;
+        }
+        &:nth-child(odd){
+          background-color: #a2a2a2;
+        }
+        &:hover {
+          background-color: #aa9619;
+          cursor: pointer;
+        }
+        td{
+          padding: 10px;
+        }
+      }
+    }
+  }
+`;
+
+const thead = ["Valor", "Fecha", "Tipo", "Concepto"];
+
 export const ExpenseTable = () => {
-
-const thead = ["Valor", "Concepto", "Fecha", "Tipo"]
-
-  return(
-    <main>
-    <h1>Tabla Gastos</h1>
-    <Table thead={thead} tbody={entry}/>
-    </main>
-  )
-}
+  return (
+    <ExpenseTableStyle>
+      <h1>Tabla Gastos</h1>
+      <Table thead={thead} tbody={entry} />
+    </ExpenseTableStyle>
+  );
+};
