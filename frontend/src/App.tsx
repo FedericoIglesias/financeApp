4bbdc8689490d31416fa41abbb059bfbe82dbe94
FedericoIglesias <igl.fede@gmail.com>
2025-01-31 16:58:08 +0100

    AddEntrance

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,78 @@
-import {useState} from 'react';
-import logo from './assets/images/logo-universal.png';
-import './App.css';
-import {Greet} from "../wailsjs/go/main/App";
+import styled from "styled-components";
 
-function App() {
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
-    const [name, setName] = useState('');
-    const updateName = (e: any) => setName(e.target.value);
-    const updateResultText = (result: string) => setResultText(result);
+const Body = styled.body`
+  background-color: #f1f1f1;
+  height: 100dvh;
+  display: grid;
+  grid-template-columns: 1fr 4fr;
+  font-family: Arial, sans-serif;
+  color: black;
+`;
 
-    function greet() {
-        Greet(name).then(updateResultText);
+const Aside = styled.aside`
+  padding: 20px;
+  margin: 20px 0px;
+  font-size: 18px;
+  border-right: 2px solid black;
+  ul {
+    padding: 10px;
+  }
+  ul:hover {
+    cursor: pointer;
+    background-color: #d1d1d1;
+  }
+`;
+
+const AddEntrance = styled.main`
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  input{
+    margin-bottom: 10px;
+    width: 200px;
+  }
+  select{
+    margin-bottom: 10px;
+    width: 200px;
+  }
+  button{
+    width: 100px;
+    padding: 10px;
+    background-color: #f1f1f1;
+    border: 1px solid black;
+    border-radius: 5px;
+    &:hover{
+      cursor: pointer;
+      background-color: #d1d1d1;
     }
+  }
+`;
 
-    return (
-        <div id="App">
-            <img src={logo} id="logo" alt="logo"/>
-            <div id="result" className="result">{resultText}</div>
-            <div id="input" className="input-box">
-                <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
-                <button className="btn" onClick={greet}>Greet</button>
-            </div>
-        </div>
-    )
+export function App() {
+  return (
+    <Body>
+      <Aside>
+        <ul>Agregar Entrada</ul>
+        <ul>Tabla Gastos</ul>
+        <ul></ul>
+        <ul></ul>
+        <ul></ul>
+      </Aside>
+      <AddEntrance>
+        <h1>Agregar Entrada</h1>
+        <label htmlFor="">Valor</label>
+        <input type="number" name="" id="" />
+        <label htmlFor="">Concepto:</label>
+        <input type="text" name="" id="" />
+        <label htmlFor="">Fecha:</label>
+        <input type="date" name="" id="" />
+        <select name="" id="">
+          <option value=""></option>
+          <option value="">Gasto</option>
+          <option value="">Ingreso</option>
+        </select>
+        <button>Guardar</button>
+      </AddEntrance>
+    </Body>
+  );
 }
-
-export default App
