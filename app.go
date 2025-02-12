ab0dd2f68e22e521a92463af05cf09cd00a06198
FedericoIglesias <igl.fede@gmail.com>
2025-02-12 13:04:27 +0100

    featGetAllExpenses

--- app.go
+++ app.go
@@ -17,7 +17,7 @@ type App struct {
 func (a *App) initDB() (*local_db.Driver, error) {
 	driver, err := local_db.New("db", nil)
 	if err != nil {
-		return nil, fmt.Errorf("Error creating database: %v", err)
+		return nil, fmt.Errorf("error creating database: %v", err)
 	}
 	return driver, nil
 }
@@ -40,7 +40,7 @@ func (a *App) RegisterTransaction(transaction model.Transaction) (bool, error) {
 
 	err = driver.Write("transactions", transaction.ID, transaction)
 	if err != nil {
-		return false, fmt.Errorf("Error writing transaction: %v", err)
+		return false, fmt.Errorf("error writing transaction: %v", err)
 	}
 
 	return true, nil
@@ -71,4 +71,32 @@ func (a *App) ReadAllIncomes() ([]model.Transaction, error) {
 	}
 
 	return incomes, nil
+}
+
+func (a *App) ReadAllExpenses() ([]model.Transaction, error) {
+	driver, err := a.initDB()
+	if err != nil {
+		return nil, fmt.Errorf("error reading transactions: %v", err)
+	}
+
+	transactions, err := driver.ReadAll("transactions")
+	if err != nil {
+		return nil, fmt.Errorf("error reading transactions: %v", err)
+	}
+
+	var expense []model.Transaction
+
+	for _, transaction := range transactions {
+		var t model.Transaction
+		err := json.Unmarshal([]byte(transaction), &t)
+		if err != nil {
+			return nil, fmt.Errorf("error unmarshalling transaction: %v", err)
+		}
+		if t.Type == "expense" {
+			expense = append(expense, t)
+		}
+	}
+
+	return expense, nil
+
 }
